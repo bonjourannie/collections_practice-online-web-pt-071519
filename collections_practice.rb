26af81e9633ae8330ca90480f49f13af532c42a8
Annie Truex <artruex@gmail.com>
2019-07-26 20:34:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,12 +2,12 @@ def sort_array_asc(array)
   array.sort
 end
 
-def sory_array_desc(array)
+def sort_array_desc(array)
   array.sort do |left, right|
     right <=> left 
   end
 end
 
-def sory_array_char_count
+def sort_array_char_count
   
 end
